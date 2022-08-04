2813a8dec890a070d9654bdfb5dc1276773a6bc5
Ahriana <ahrianadev@gmail.com>
2022-08-04 23:14:54 +0100

    change static path

--- build/prepare-swagger-ui.js
+++ build/prepare-swagger-ui.js
@@ -9,7 +9,7 @@ const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 const copyFile = promisify(fs.copyFile);
 
-const STATIC_DIR = './static';
+const STATIC_DIR =  path.join(__dirname, '..', 'static');
 
 (async () => {
   try {
